3f3e60dc9ebb87b94fadc011ef7c3c00a06a07c4
bezuidenhoutxena <bezuidenhoutxena@gmail.com>
2026-02-05 09:57:14 +0100

    Resolve merge conflict

---
+++ particle.py
@@ -0,0 +1,8 @@
+# Exercise 1.3.1
+
+# Resolved version
+class Particle:
+    def __init__(self, mass, position):
+        self.mass = mass
+        self.position = position
+        self.velocity = [0.0, 0.0, 0.0]
